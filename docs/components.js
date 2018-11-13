702cfcbec582ce88253814c76be4582ad6e7b09f
Brent Jackson <jxnblk@gmail.com>
2018-11-13 19:10:38 +0000

    Rewrite with all CSS properties instead of styled-system

--- docs/components.js
+++ docs/components.js
@@ -1,22 +1,17 @@
 import React from 'react'
-import RebassMDX from '@rebass/mdx'
-import { Link } from 'react-router-dom'
 import Box from '../src'
 
 const components = {
   Box,
-  Link,
 }
 
 export const Root = props =>
-  <RebassMDX components={components}>
-    <Box
-      mx='auto'
-      px={3}
-      py={4}
-      css={{
-        maxWidth: '1024px'
-      }}>
-      {props.children}
-    </Box>
-  </RebassMDX>
+  <Box
+    mx='auto'
+    px={3}
+    py={4}
+    css={{
+      maxWidth: '1024px'
+    }}>
+    {props.children}
+  </Box>
